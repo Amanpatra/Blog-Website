bf3d47b58628b6055f809e9b9243b8669bbed54c
Aman Patra <amanpatra21@gmail.com>
2020-08-18 23:48:51 +0530

    version:2 - Add Database to the Blog-site

--- app.js
+++ app.js
@@ -1,71 +1,97 @@
 //jshint esversion:6
 
+// npm modules-----------------------------------
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
+// ----------------------------------------------
 
+
+// Starting content of default pages-------------
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+// ----------------------------------------------
+
 
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+
+// Connecting to Database------------------------
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+// ----------------------------------------------
 
 
+// Schema and model of the Database--------------
+const postSchema = {
+    title: String,
+    body: String
+}
+
+const Post = mongoose.model("Post", postSchema);
+// ----------------------------------------------
+
+
+// All routes for get and post menthod-----------
 app.get("/", function(req, res) {
-    res.render("home", {
-        homeStartingContent: homeStartingContent,
-        posts: posts
-    });
+    Post.find(function(err, foundList) {
+        if(!err){
+            res.render("home", {
+                homeStartingContent: homeStartingContent,
+                posts: foundList
+            });
+        }
+    })
+    
 })
-
 
 app.get("/about", function(req, res) {
     res.render("about", {aboutContent: aboutContent});
 })
 
-
 app.get("/contact", function(req, res) {
     res.render("contact", {contactContent: contactContent});
 })
 
+app.get("/post/:postId", function(req, res) {
+    const searchPostId = req.params.postId;
 
-app.get("/post/:postName", function(req, res) {
-    const searchPostName = _.lowerCase(req.params.postName);
-
-    posts.forEach(function(post) {
-        if ( _.lowerCase(post.title) === searchPostName ) {
+    Post.findById(searchPostId, function(err, foundPost) {
+        if(!err) {
             res.render("post", {
-                postTitle: post.title,
-                postBody: post.body
-            
+                postTitle: foundPost.title,
+                postBody: foundPost.body            
             })
         }
     })
 })
 
-
 app.get("/compose", function(req, res) {
     res.render("compose");
 })
+
 app.post("/compose", function(req, res) {
-    const newPost = {
+    const newPost = new Post({
         title: req.body.postTitle,
         body: req.body.postBody
-    }
-    posts.push(newPost);
-    res.redirect("/")
+    })
+    newPost.save(function(err) {
+        if(!err) {
+            res.redirect("/");
+        }
+    });
+    
 })
-
-
-
+// ----------------------------------------------
 
 
 app.listen(3000, function() {
